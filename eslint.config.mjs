219f72520e1f749fdc9588692bc8fcf580c131a1
Pudit Yensap <pudit.ye@prtr.com>
2026-09-04 15:59:28 +0700

    fix(dx): recover git index and configure vscode settings to suppress noisy tab badges

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,6 +13,13 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@next/next/no-img-element": "off",
+      "@next/next/no-location-assign-relative-destination": "warn",
+    },
+  },
 ]);
 
 export default eslintConfig;
